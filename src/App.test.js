0293e96b73a2d46d6e8bef116c811f3be888a937
anderCM <andercm15@gmail.com>
2023-02-23 10:12:03 -0500

    Component test added and extra tests for calculate and operate

--- src/App.test.js
+++ src/App.test.js
@@ -4,3 +4,28 @@ import ErrorPage from './components/ErrorPage';
 import Home from './components/Home';
 import Quote from './components/Quote';
 import RouterNav from './Router';
+
+test('Test if RouterNav is well build', () => {
+  render(<RouterNav />);
+  expect(screen.getByText('Math Magicians')).toBeInTheDocument();
+});
+
+test('Test if Calculator is well build', () => {
+  render(<Calculator />);
+  expect(screen.getByText('Lets do some math!')).toBeInTheDocument();
+});
+
+test('Test if Home is well build', () => {
+  render(<Home />);
+  expect(screen.getByText('Welcome to our main page!')).toBeInTheDocument();
+});
+
+test('Test if Quote is well build', () => {
+  render(<Quote />);
+  expect(screen.getByText('Pure mathematics is, in its way, the poetry of logical ideas.')).toBeInTheDocument();
+});
+
+test('Test if ErrorPage is well build', () => {
+  render(<ErrorPage />);
+  expect(screen.getByText('Ups...Something went wrong!')).toBeInTheDocument();
+});
